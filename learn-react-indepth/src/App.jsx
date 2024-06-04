df7b0ff1eea5aff47b0b18d7bad86a7d91cf5f76
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-04 15:54:15 +0500

    refactor: Use class based component

--- learn-react-indepth/src/App.jsx
+++ learn-react-indepth/src/App.jsx
@@ -1,44 +1,74 @@
 /* eslint-disable no-unused-vars */
-import { Fragment, useState } from "react";
-function App() {
-  const [inc, setInc] = useState(0);
-  const [dec, setDec] = useState(0);
 
-  const IncrementCounter = () => {
-    // setInc(inc + 1);
-    setInc(inc + 1);
-    setInc((prev) => prev + 1);
-    setInc((prev) => prev + 1);
+//* functional
+// import { Fragment, useState } from "react";
+// function App() {
+//   const [inc, setInc] = useState(0);
+//   const [dec, setDec] = useState(0);
 
-    console.log(inc);
-  };
-  const decrementCounter = () => {
-    setDec(dec - 1);
-  };
-  return (
-    <Fragment>
+//   const IncrementCounter = () => {
+//     // setInc(inc + 1);
+//     setInc(inc + 1);
+//     setInc((prev) => prev + 1);
+//     setInc((prev) => prev + 1);
+
+//     console.log(inc);
+//   };
+//   const decrementCounter = () => {
+//     setDec(dec - 1);
+//   };
+//   return (
+//     <Fragment>
+//       <div>
+//         <button
+//           onClick={IncrementCounter}
+//           className="border 1px solid bg-green-600 p-1 cursor-pointer"
+//         >
+//           Increment Counter {inc}
+//         </button>
+//       </div>
+//       <button
+//         onClick={decrementCounter}
+//         className="border 1px solid bg-green-600 p-1 cursor-pointer"
+//       >
+//         Decrement Counter {dec}
+//       </button>
+//     </Fragment>
+//   );
+// }
+
+// //* Calling a function!
+// // console.log(App());
+
+// //* Calling a component!
+// // console.log(<App />);
+
+//* class based
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((state) => {
+      return { count: state.count + 1 };
+    });
+  }
+  render() {
+    return (
       <div>
-        <button
-          onClick={IncrementCounter}
-          className="border 1px solid bg-green-600 p-1 cursor-pointer"
-        >
-          Increment Counter {inc}
+        <button key={1} onClick={this.handleClick}>
+          Click me!
         </button>
+        <span key={2}>{this.state.count}</span>
       </div>
-      <button
-        onClick={decrementCounter}
-        className="border 1px solid bg-green-600 p-1 cursor-pointer"
-      >
-        Decrement Counter {dec}
-      </button>
-    </Fragment>
-  );
+    );
+  }
 }
 
-//* Calling a function!
-// console.log(App());
-
-//* Calling a component!
-// console.log(<App />);
-
-export default App;
+// export default App;

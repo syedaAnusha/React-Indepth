b1c53283d1b2f3b24d9c545c361271e92d32923a
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-07 01:34:45 +0500

    refactor: Implement Updating phase

--- learn-react-indepth/src/LifeCycleA.jsx
+++ learn-react-indepth/src/LifeCycleA.jsx
@@ -20,12 +20,22 @@ class LifeCycleA extends Component {
     console.log("LifeCycleA componentDidMount!");
   }
 
+  shouldComponentUpdate() {
+    console.log("LifeCycleA ShouldComponentUpdate!");
+    return true;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("LifeCycleA getSnapShotBeforeUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("LifeCycleA componentDidUpdate!");
+  }
   render() {
     console.log("LifeCycleA render Method called!");
     return (
       <div className="text text-cyan-800 font-extrabold text-2xl">
-        Hey I am LifeCycle A
-        <LifeCycleAChild />
+        Hey I am LifeCycle A{/* <LifeCycleAChild /> */}
       </div>
     );
   }

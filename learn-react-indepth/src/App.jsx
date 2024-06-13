00d3dd0a5febf25b62cc6a0b80f318cb62c44fe9
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-13 17:00:59 +0500

    refactor: Add LocalMutation.jsx

--- learn-react-indepth/src/App.jsx
+++ learn-react-indepth/src/App.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
-import Counter from "./react-hooks-components/use-state/Counter";
+import LocalMutation from "./react-hooks-components/use-state/LocalMutation";
 export default class App extends Component {
   render() {
     return (
       <>
-        <Counter />
+        <LocalMutation />
       </>
     );
   }
---
+++ learn-react-indepth/src/react-hooks-components/use-state/LocalMutation.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+function Cup({ guest }) {
+  return <h2 className="text-white">Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaGathering() {
+  let cups = [];
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />);
+  }
+  return cups;
+}

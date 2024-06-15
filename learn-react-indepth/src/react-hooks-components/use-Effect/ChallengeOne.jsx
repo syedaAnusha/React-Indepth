c2ff2a2e98c8fa5ad46bc1420365096878f8b718
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-15 18:19:10 +0500

    refactor

--- learn-react-indepth/src/react-hooks-components/use-Effect/ChallengeOne.jsx
+++ learn-react-indepth/src/react-hooks-components/use-Effect/ChallengeOne.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+
+import { useState, useEffect } from "react";
+const colorSet = [
+  "bg-teal-800",
+  "bg-cyan-400",
+  "bg-slate-600",
+  "bg-zinc-800",
+  "bg-green-800",
+  "bg-amber-800",
+  "bg-orange-800",
+  "bg-purple-800",
+  "bg-pink-800",
+  "bg-gray-800",
+];
 
 const ChallengeOne = () => {
-  return <div></div>;
+  const [color, setColor] = useState(colorSet);
+
+  function changeColor() {
+    const number = Math.floor(Math.random() * 10);
+    setColor(colorSet[number]);
+  }
+  useEffect(() => {
+    let id = setInterval(changeColor, 500);
+    return () => clearInterval(id);
+  }, [color]);
+  return (
+    <div className="flex justify-center items-center gap-5">
+      <h1 className="text-white text-3xl font-bold text-center">
+        Hey!! I am useEffect!
+      </h1>
+      <button className="p-2 rounded-md text-white bg-gray-900 bg cursor-pointer">
+        Click me to see magic!!
+      </button>
+
+      <div
+        className={`w-80 h-80 items-center mt-4 ${color} rounded-md bg-`}
+      ></div>
+    </div>
+  );
 };
 
 export default ChallengeOne;

7997c881f2b71f648cffa6641f14cdbc66b255ec
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-06-15 17:39:43 +0500

    refactor: Add use-Effect folder

--- learn-react-indepth/src/App.jsx
+++ learn-react-indepth/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
-import ChallengeOne from "./react-hooks-components/use-state/useState-Queue/ChallengeOne";
+import ChallengeOne from "./react-hooks-components/use-Effect/ChallengeOne";
 export default class App extends Component {
   render() {
     return (
---
+++ learn-react-indepth/src/react-hooks-components/use-Effect/ChallengeOne.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChallengeOne = () => {
+  return <div></div>;
+};
+
+export default ChallengeOne;

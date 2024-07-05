d348386b3b323d0b5f0bb91d98ccf6972a8f2696
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-07-05 23:52:57 +0500

    feat: Add heading

--- learn-react-indepth/src/react-hooks-components/use-Context/Form.jsx
+++ learn-react-indepth/src/react-hooks-components/use-Context/Form.jsx
@@ -2,7 +2,8 @@ import Panel from "./Panel";
 import Button from "./Button";
 const Form = () => {
   return (
-    <div>
+    <div className="text-5xl text-white">
+      I am an example of USE-CONTEXT
       <Panel mode="dark">
         <Button>Sign Up</Button>
         <Button>Login </Button>

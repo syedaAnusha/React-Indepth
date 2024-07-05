7996ff10015d59f8b4b6977b1d81092a04ec5295
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-07-05 23:54:11 +0500

    style

--- learn-react-indepth/src/react-hooks-components/use-Context/Form.jsx
+++ learn-react-indepth/src/react-hooks-components/use-Context/Form.jsx
@@ -2,8 +2,8 @@ import Panel from "./Panel";
 import Button from "./Button";
 const Form = () => {
   return (
-    <div className="text-5xl text-white">
-      I am an example of USE-CONTEXT
+    <div className="text-5xl text-white flex-col justify-center items-center">
+      <h1>I am an example of USE-CONTEXT</h1>
       <Panel mode="dark">
         <Button>Sign Up</Button>
         <Button>Login </Button>
